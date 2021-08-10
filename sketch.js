a2e331b434e386abd6fb11f9af89ccb1bf49962f
Aaryan-17Jaiswal <85996672+Aaryan-17Jaiswal@users.noreply.github.com>
2021-08-10 14:51:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var cloud_moving;
+
+
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloud_moving = loadAnimation("cloud.png");
+ 
+  
+}
+
+function setup() {
+
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //create a ground sprite
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //creating invisible ground
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+   
+}
+
+function spawnClouds(){
+
+  if(frameCount%60===0){
+    var cloud = createSprite(600,50,50,50);
+    cloud.y = Math.round(random(10,80));
+    cloud.velocityX = -2;
+    cloud.addAnimation("moving", cloud_moving);
+    cloud.scale = 0.5
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+  
+  
+  }
+  
+  
+
+
+}
+
+
+
+function draw() {
+  //set background color
+  background(180);
+  
+  //console.log(trex.y)
+  
+  
+  
+  // jump when the space key is pressed
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //stop trex from falling down
+  trex.collide(invisibleGround);
+  
+  //Spawn Clouds
+  spawnClouds()
+  
+  console.log(frameCount);
+
+  drawSprites();
+}
+
+
+
+
+
